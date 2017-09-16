6aa1156f938d0cf3d761611522b731833906cfb4
涂健 <961442649@qq.com>
2017-09-16 08:58:30 +0800

    Update java.java

--- src/org/lins/java/java.java
+++ src/org/lins/java/java.java
@@ -5,7 +5,7 @@ public class java {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("1234567赵钱孙李周吴郑王123你好啊 ");
-System.out.println("再来一次123456 ");
+System.out.println("再来一次12345678 ");
 	}
 
 }
